a0a2632787665b258807a1804e8a5dbaa843a292
JOJO <18510829974@163.com>
2018-08-28 18:56:48 +0800

    添加可下拉刷新和上拉加载更多的LRecyclerView及设置样式

--- app/src/main/java/com/smart/novel/ui/ACT_Login.kt
+++ app/src/main/java/com/smart/novel/ui/ACT_Login.kt
@@ -1,10 +1,16 @@
 package com.smart.novel.ui
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import butterknife.OnClick
+import com.github.jdsjlzx.interfaces.OnLoadMoreListener
+import com.github.jdsjlzx.interfaces.OnRefreshListener
+import com.github.jdsjlzx.recyclerview.LRecyclerViewAdapter
+import com.github.jdsjlzx.recyclerview.ProgressStyle
 import com.smart.framework.library.bean.ErrorBean
+import com.smart.novel.MyApplication
 import com.smart.novel.R
 import com.smart.novel.adapter.ADA_SearchList
 import com.smart.novel.adapter.ADA_TestMultiple
@@ -21,9 +27,11 @@ import kotlinx.android.synthetic.main.act_login.*
  * wechat:18510829974
  * description: 登录页面
  */
-class ACT_Login : BaseMVPActivity<TestPresenter, TestModel>(), TestContract.View {
+class ACT_Login : BaseMVPActivity<TestPresenter, TestModel>(), TestContract.View, OnRefreshListener, OnLoadMoreListener {
     var mAdapter: ADA_SearchList? = null
     var mMutiAdapter: ADA_TestMultiple? = null
+    var mLrecyclViewAdapter: LRecyclerViewAdapter? = null
+
     var data = ArrayList<UserEntity>()
 
     override fun getBundleExtras(extras: Bundle?) {
@@ -33,8 +41,21 @@ class ACT_Login : BaseMVPActivity<TestPresenter, TestModel>(), TestContract.View
     override fun startEvents() {
 //        mAdapter = ADA_SearchList(this)
         mMutiAdapter = ADA_TestMultiple(this)
+        mLrecyclViewAdapter = LRecyclerViewAdapter(mMutiAdapter)
+        recyclerview.adapter = mLrecyclViewAdapter
+        recyclerview.setHasFixedSize(true)
         recyclerview.layoutManager = LinearLayoutManager(this)
-        recyclerview.adapter = mMutiAdapter
+        recyclerview.setRefreshProgressStyle(ProgressStyle.TriangleSkewSpin)
+//        recyclerview.setLoadingMoreProgressStyle(ProgressStyle.SysProgress);
+        recyclerview.setLoadingMoreProgressStyle(ProgressStyle.BallTrianglePath);
+        recyclerview.setOnRefreshListener(this)
+        recyclerview.setOnLoadMoreListener(this)
+        //设置头部文字颜色
+        recyclerview.setHeaderViewColor(R.color.color_3AC270, R.color.color_73787d, R.color.color_f9f9f9)
+        //设置底部加载颜色-loading动画颜色,文字颜色,footer的背景颜色
+        recyclerview.setFooterViewColor(R.color.color_3AC270, R.color.color_73787d, R.color.color_f9f9f9)
+        //设置底部加载文字提示
+        recyclerview.setFooterViewHint(MyApplication.context.getString(R.string.list_footer_loading), MyApplication.context.getString(R.string.list_footer_end), MyApplication.context.getString(R.string.list_footer_network_error))
 
         for (i in 0..50) {
             var user = UserEntity()
@@ -42,6 +63,14 @@ class ACT_Login : BaseMVPActivity<TestPresenter, TestModel>(), TestContract.View
             user.name = "item=" + i
             data.add(user)
         }
+    }
+
+    override fun onRefresh() {
+        Handler().postDelayed(Runnable { recyclerview.refreshComplete(1) }, 2000)
+    }
+
+    override fun onLoadMore() {
+        Handler().postDelayed(Runnable { recyclerview.setNoMore(true) }, 2000)
     }
 
     override fun showBusinessError(error: ErrorBean?) {
@@ -58,7 +87,7 @@ class ACT_Login : BaseMVPActivity<TestPresenter, TestModel>(), TestContract.View
             R.id.tv_do_search -> {
                 mMvpPresenter.getTestData(multipleStatusView)
             }
-            R.id.et_search_keywords -> mAdapter!!.update(data.subList(0, 10), true)
+            R.id.et_search_keywords -> mMutiAdapter!!.update(data.subList(0, 10), true)
         }
     }
 
